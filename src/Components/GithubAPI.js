245216a78f0659f76c7b12c36a741239a93ad0da
Sandeep <95558185+sandeep7032@users.noreply.github.com>
2023-06-22 20:37:13 +0530

    GithubAPI.js

--- src/Components/GithubAPI.js
+++ src/Components/GithubAPI.js
@@ -1,18 +1,14 @@
 import axios from 'axios'
 
 const getUserData = (username, addData) => {
-    if (username === '') {
-        alert("Enter User Id")
-    } else {
-        axios.get(`https://api.github.com/users/${username}`)
-            .then((res) => {
-                console.log(res.data)
-                addData(res.data)
-            })
-            .catch(() => {
-                alert("Failed to Load")
-            })
-    }
+    axios.get(`https://api.github.com/users/${username}`)
+        .then((res) => {
+            console.log(res.data)
+            addData(res.data)
+        })
+        .catch(() => {
+            alert("Failed to Load")
+        })
 }
 
 export default getUserData
